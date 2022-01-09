7fdae9192cc4b9c26855dc86df63705d34a6d1ce
MichaelLuo520 <po2319987@gmail.com>
2022-01-09 22:14:01 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!123123s" />
+      <HelloWorld msg="You did it!123123s123123" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
